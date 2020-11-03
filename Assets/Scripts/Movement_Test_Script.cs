42fe413f7c81df37ff0b4a5b403f3a79e039b5dd
chickenproductions <chickenproductions@users.noreply.github.com>
2020-11-02 19:38:56 -0800

    Rumentary movement test
    test script with movement, using set velocity, may switch out for using the transform version instead if it works well with collision due to the fact of when you need to rotate on walls

---
+++ Assets/Scripts/Movement_Test_Script.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement_Test_Script : MonoBehaviour
+{
+    
+    public PlayerMovement playerMovement;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float input = Input.GetAxis("Horizontal");
+        float Speed = playerMovement.Speed;
+        float AngularSpeed = playerMovement.AngularSpeed;
+
+        //rb.AddTorque(Speed *input);
+        rb.angularVelocity = (AngularSpeed * -input);
+        rb.velocity = new Vector2(Speed * input, rb.velocity.y );
+
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/Actual Scripts/PlayerMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "PlayerMovementData", menuName = "Data Components/PlayerMovementComponent", order = 1)]
+public class PlayerMovement : ScriptableObject
+{
+    public float Speed = 5;
+    public float AngularSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+}
