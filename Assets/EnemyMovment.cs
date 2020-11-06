32ed6eab50115de44ff2c19d519d325f6a345905
chickenproductions <chickenproductions@users.noreply.github.com>
2020-11-05 20:24:55 -0800

    initial learning of pathfinding kindof
    will learn more about A* also refactoring will be needed into a generalised "Pathfinding script" that will handle the actual pathfinding

---
+++ Assets/EnemyMovment.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyMovment : MonoBehaviour
+{
+    public Transform TargetTemp;
+    public float speed;
+    public float nextpointDst;
+
+    Path path;
+    int currentWaypoint = 0;
+    bool reachedEndOfPath = false;
+
+    Seeker seeker;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+        InvokeRepeating("UpdatePath", 0f, 0.5f);
+        
+    }
+
+    void UpdatePath()
+    {
+        if(seeker.IsDone())
+        seeker.StartPath(rb.position, TargetTemp.position, OnPathComplete);
+    }
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (path == null)
+            return;
+
+        if(currentWaypoint >= path.vectorPath.Count)
+        {
+            reachedEndOfPath = true;
+            return;
+
+        }
+        else
+        {
+            reachedEndOfPath = false;
+        }
+        
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+        Vector2 movementspeed = direction * speed * Time.deltaTime;
+        
+        rb.velocity += movementspeed;
+        
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+
+        if(distance < nextpointDst)
+        {
+            currentWaypoint++;
+        }
+    }
+}
